c1c04374df17a57693208096bf1d6cba45cd7929
Private-Kawa <voidout76@gmail.com>
2023-01-14 21:04:23 +0900

    Breeze ナビゲーションリンクの修正

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/', [PostController::class, 'index']);
+    Route::get('/', [PostController::class, 'index'])->name('/');
     Route::post('/posts',  [PostController::class, 'store']);
     Route::get('/posts/create',  [PostController::class, 'create']);
     Route::get('/posts/{post}',  [PostController::class, 'show']);
