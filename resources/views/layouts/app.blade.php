fc61f53341deef67f46bfbcd0d68d6926b135704
Private-Kawa <voidout76@gmail.com>
2023-01-14 21:21:05 +0900

    Breeze ナビゲーションの修正 レスポンシブレイアウト

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,14 +20,14 @@
             <!-- Page Heading -->
             @if (isset($header))
                 <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    <div class="container max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
                 </header>
             @endif
 
             <!-- Page Content -->
-            <main>
+            <main class="container mx-auto px-4 max-w-7xl sm:px-6 lg:px-8">
                 
                 {{ $slot }}
                 
